e65e5d4cd753e3e1a43fedda30b9bdcc6d323ead
skm <skm@hubble.kr>
2018-01-29 12:21:16 +0000

    이미지 크롭 기능 개발중

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const fs = require('fs');
+const router = express.Router();
+const sysAttcFile = require(__base + '/model/sys/sysAttcFile.js');
 
 /** 메인 화면 */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Baton 2.0' });
+});
+
+/** 파일 보기 */
+router.get("/fileView/:fileAttcSeq", function(req, res, next) {
+    
+    let fileAttcSeq = req.params.fileAttcSeq;
+    
+    sysAttcFile.select(fileAttcSeq, function(err, result) {
+        
+        if (err) {
+            res.send("");
+        } else {
+            
+            if(null == result) {
+                res.send("");
+            } else {
+            
+                res.setHeader("content-type", result["MIME_TYPE"]);
+                fs.createReadStream(result["FILE_PATH"]).pipe(res);
+            }
+        }
+        
+    });
+});
+
+/** 이미지 보기 */
+router.get("/imgView/:fileAttcSeq", function(req, res, next) {
+    
+    let fileAttcSeq = req.params.fileAttcSeq;
+    
+    sysAttcFile.select(fileAttcSeq, function(err, result) {
+        
+        if (err) {
+            res.send("");
+        } else {
+            
+            if(null == result) {
+                res.send("");
+            } else if (result["MIME_TYPE"].indexOf("image") == 0) {
+            
+                res.setHeader("content-type", result["MIME_TYPE"]);
+                fs.createReadStream(result["FILE_PATH"]).pipe(res);
+            } else {
+                res.send("");
+            }
+        }
+        
+    });
 });
 
 module.exports = router;
